99b3253e4071179ac45a0e75aa39fa4a04b4b9e3
Raunak Lanjewar <raunaklanjewar42@gmail.com>
2024-10-01 02:47:46 +0530

    Header created.

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,5 @@
+export const Header:React.FC = ()=>{
+    return<div className="w-full h-[80px] p-8  flex items-center justify-start text-[#E0FF00]" >
+        <h1 className="text-4xl" >BB.</h1>
+    </div>
+}

331ff765ccc43632d153b2c10d67f67ec7445b28
Raunak Lanjewar <raunaklanjewar42@gmail.com>
2024-10-01 17:57:12 +0530

    Pricing component added to page.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 import { Hero } from "@/components/Hero/Hero";
+import Pricing from "@/components/Pricing/Pricing";
 
 export default function Home() {
   return (
     <div className="w-full h-full overflow-x-clip">
       <Hero />
-      <div className="h-screen w-screen bg-black" ></div>
+      <div className="h-screen w-screen bg-black" >
+        <Pricing/>
+      </div>
     </div>
   );
 }

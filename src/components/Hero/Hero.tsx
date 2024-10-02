0981ff23a6ac6a8027abca9fbe7ac66ce6a0c8b2
Raunak Lanjewar <raunaklanjewar42@gmail.com>
2024-10-02 09:42:52 +0530

    Importing Spline from "@splinetool/react-spline" vs "@splinetool/react-spline/next" fixes the error where clicking on the header buttons gave problems.

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,5 +1,4 @@
-"use client";
-import Spline from "@splinetool/react-spline/next";
+import Spline from "@splinetool/react-spline";
 import { Header } from "../Header/Header";
 import IntroModal from "../Modal/Modal";
 import { Button } from "../Button/Button";
@@ -20,27 +19,31 @@ export const Hero: React.FC<HeroProps> = ({
   return (
     <div className="w-screen h-screen relative z-10 bg-black">
       <Spline scene="https://prod.spline.design/UbPpaC5c8Ixgkqry/scene.splinecode" />
-      <div className="absolute top-0 h-full w-full bg-black bg-opacity-0 ">
+      <div className="absolute top-0 left-0 w-full h-full pointer-events-none">
         <IntroModal />
-        <Header
-          setFeaturesCLicked={setFeaturesCLicked}
-          setPricingClicked={setPricingClicked}
-          setContactClicked={setContactClicked}
-        />
-        <div className="  pl-20 flex flex-col items-start justify-start pt-[140px] w-[50%]">
-          <h1 className="text-7xl font-bold text-[#E0FF00]  uppercase">
+        <div className="pointer-events-auto">
+          <Header
+            setFeaturesCLicked={setFeaturesCLicked}
+            setPricingClicked={setPricingClicked}
+            setContactClicked={setContactClicked}
+          />
+        </div>
+        <div className="pl-20 flex flex-col items-start justify-start pt-[140px] w-[50%]">
+          <h1 className="text-7xl font-bold text-[#E0FF00] uppercase">
             Meet Robbie, Your AI Companion
           </h1>
           <p className="text-white text-2xl mt-4">
             Experience the future of personal robotics with Robbie, your
             intelligent and versatile AI companion.
           </p>
-          <Button onClick={() => setPricingClicked(true)} className="mt-8">
-            Pre Order Now
-          </Button>
+          <div className="pointer-events-auto">
+            <Button onClick={() => setPricingClicked(true)} className="mt-8">
+              Pre Order Now
+            </Button>
+          </div>
         </div>
         <div className="w-full flex items-center justify-center">
-          <FaAnglesDown className=" size-8  mt-[140px]" color="#ffffff" />
+          <FaAnglesDown className="size-8 mt-[140px]" color="#ffffff" />
         </div>
       </div>
     </div>
